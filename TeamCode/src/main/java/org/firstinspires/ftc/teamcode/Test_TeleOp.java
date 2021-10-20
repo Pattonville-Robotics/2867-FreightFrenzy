2056dbc9eaa613c9b888679db9f5d158d0bade72
Adedeji Coker <cokera01@student.psdr3.org>
2021-10-20 14:59:22 -0500

    This commit adds new test files for Test Robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_TeleOp.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class Test_TeleOp extends OpMode {
+    DcMotor left;
+    DcMotor right;
+
+    public void init() {
+        left = hardwareMap.dcMotor.get("left");
+        right = hardwareMap.dcMotor.get("right");
+
+    }
+
+    public void loop() {
+        right.setPower(-gamepad1.right_stick_y);
+        left.setPower(gamepad1.left_stick_y);
+    }
+}
