c77cf36b24591c19e0343396a13e735667534f28
pandeys01 <pandeys01@student.psdr3.org>
2021-10-26 16:07:44 -0500

    There was turning added using timers in Test_Autonomous.java.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Autonomous.java
@@ -2,11 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 //Test autonomous - written by jack, testing out how to run autonomous code
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Autonomous(name="Autonomous Test", group="Autonomous")
 public class Test_Autonomous extends LinearOpMode {
 
     private DcMotor leftDrive = null;
@@ -17,21 +20,23 @@ public class Test_Autonomous extends LinearOpMode {
         // Declare any local / helper variables here
 
         // Our initialization code should go here
-        leftDrive  = hardwareMap.get(DcMotor.class, "left");
+        leftDrive = hardwareMap.get(DcMotor.class, "left");
         rightDrive = hardwareMap.get(DcMotor.class, "right");
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         long start = System.currentTimeMillis();
+        for (int i = 0; i<4; i++) {
+            leftDrive.setPower(-0.5);
+            rightDrive.setPower(0.5);
 
-        leftDrive.setPower(0.5);
-        rightDrive.setPower(-0.5);
+            sleep(3000);
 
-        sleep(2000);
+            leftDrive.setPower(.7);
+            rightDrive.setPower(.7);
 
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
-
+            sleep(3300);
+        }
     }
 }
